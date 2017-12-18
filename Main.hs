670356ddc9c72f55a0aeff59d28ad91156973aaa
Gustavo Monteiro Alves <gugamalves@yahoo.com.br>
2017-12-18 14:46:30 -0300

    Add Main to generate JSON file

--- Main.hs
+++ Main.hs
@@ -1,4 +1,23 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import Test.HUnit
+import BagTest(tests)
+
+main = do
+        print "Digite sua matricula: "
+        matricula <- getLine
+
+        results <- runTestTT tests
+
+        let totalTestes = cases results
+        let tentativas = tried results
+        let erros = errors results
+        let falhas = failures results
+        let passaram = tentativas - (erros + falhas)
+        let jsonString = "{" ++ "\"matricula\":" ++ show matricula ++ ", "
+                             ++ "\"totalTestes\":" ++ show totalTestes ++ ", "
+                             ++ "\"erros\":" ++ show erros ++ ", "
+                             ++ "\"falhas\":" ++ show falhas ++ ", "
+                             ++ "\"passaram\":" ++ show passaram ++ "}"
+
+        writeFile "test-output.json" (jsonString)
